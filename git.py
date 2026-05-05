4eb9c6a2c6fe975321b829fc2f34bd5fa507ac05
aurora <jnasna2000@gmail.com>
2026-05-05 12:47:45 +0530

    Product of two numbers

--- git.py
+++ git.py
@@ -3,3 +3,7 @@ a=int(input("Enter First Number:"))
 b=int(input("Enter Second Number:"))
 c=a+b
 print(f"sum of {a} and {b} is: {c}")
+
+#Multiplication of two numbers
+d=a*b
+print(f"product of {a} and {b} is: {d}")
